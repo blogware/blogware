c31b2666291e7ad9549b70e47062c48013c399a0
Leo Wong <gnowoel@gmail.com>
2016-01-08 17:46:56 +0800

    Update routing table when traversing files

--- stream.js
+++ stream.js
@@ -1,7 +1,13 @@
 var through = require('through2');
+var route = require('./route');
 
 function stream() {
   return through.obj(function(file, _, cb) {
+    var relative = file.relative;
+    var location = relative;
+
+    route.add(relative, location);
+
     cb();
   });
 }
