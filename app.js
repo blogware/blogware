c2ff31f6c9d4c990e676d6c91da3d30736fd5e23
Leo Wong <gnowoel@gmail.com>
2016-01-08 20:35:35 +0800

    Add a simple 404 page

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var render = require('./render');
 var app = express();
 
 app.get('*', function(req, res) {
-  var location = req.originalUrl.slice(1);
+  var location = req.url.slice(1);
   var relative = route.l2r(location);
 
   render(relative, function(err, rendered) {
@@ -12,6 +12,11 @@ app.get('*', function(req, res) {
 
     res.send(rendered);
   });
+});
+
+app.use(function(err, req, res, next) {
+  res.status(404);
+  res.send('404 Not found');
 });
 
 module.exports = app;
