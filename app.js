5126b42eacb377fdb549e00861ce4b19aa0541b3
Leo Wong <gnowoel@gmail.com>
2016-01-08 19:21:52 +0800

    Update the Express app to send all files literally

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 var express = require('express');
+var route = require('./route');
+var render = require('./render');
 var app = express();
 
-app.get('/', function(req, res) {
-  res.send('Hello world!');
+app.get('*', function(req, res) {
+  var location = req.originalUrl.slice(1);
+  var relative = route.l2r(location);
+
+  render(relative, function(err, rendered) {
+    if (err) throw err;
+
+    res.send(rendered);
+  });
 });
 
 module.exports = app;
