60d067e4cc9424fb36514c15cb56c66046641e1f
Leo Wong <gnowoel@gmail.com>
2016-01-08 17:03:33 +0800

    Add a private gulp task for passing through files

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,15 @@
 var gulp = require('gulp');
 var app = require('./app');
 
-function serve() {
+function traverse() {
+  return gulp.src(['**/*', '!_site{,/**/*}'])
+    .pipe(gulp.dest('_site'));
+}
+
+function preview() {
   app.listen(3000, function() {
     console.log('Listening on port 3000');
   });
 }
 
-gulp.task('serve', serve);
+gulp.task('serve', gulp.series(traverse, preview));
