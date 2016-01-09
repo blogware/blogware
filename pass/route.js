b119f481a8109f4583253357ac74caa6d5252e52
Leo Wong <gnowoel@gmail.com>
2016-01-10 01:10:13 +0800

    Map a file relative to a URL location with the processing engine

--- pass/route.js
+++ pass/route.js
@@ -5,6 +5,12 @@ function pass(file) {
   var relative = file.relative;
   var location = relative;
 
+  var engine = file.meta('engine');
+
+  if (engine) {
+    location = engine.route(relative);
+  }
+
   route.add(relative, location);
 
   return file;
