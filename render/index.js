3dbb554db340b4502e74c444b80b0d3e814a5220
Leo Wong <gnowoel@gmail.com>
2016-01-10 00:09:47 +0800

    Update the render module to handle assets, templates or others

--- render/index.js
+++ render/index.js
@@ -7,12 +7,54 @@ function render(location, cb) {
   var file = record.r2f(relative);
 
   if (!file) {
-    return cb(new Error('File not found'));
+    return cb(new Error('File not found: ' + location));
   }
 
+  if (file.meta('type') === 'asset') {
+    renderAsset(file, function(err, rendered) {
+      if (err) return cb(err);
+
+      cb(null, rendered);
+    });
+  } else if (file.meta('type') === 'template') {
+    renderTemplate(file, function(err, rendered) {
+      if (err) return cb(err);
+
+      cb(null, rendered);
+    });
+  } else {
+    renderOther(file, function(err, rendered) {
+      if (err) return cb(err);
+
+      cb(null, rendered);
+    });
+  }
+}
+
+function renderAsset(file, cb) {
   var contents = file.contents.toString('utf8');
 
   cb(null, contents);
+}
+
+function renderTemplate(file, cb) {
+  var engine = file.meta('engine');
+
+  engine.render(file, {}, function(err, rendered) {
+    if (err) return cb(err);
+
+    cb(null, rendered);
+  });
+}
+
+function renderOther(file, cb) {
+  var engine = file.meta('engine');
+
+  engine.render(file, {}, function(err, rendered) {
+    if (err) return cb(err);
+
+    cb(null, rendered);
+  });
 }
 
 module.exports = render;
