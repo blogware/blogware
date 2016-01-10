46f7b8847a68d90f1e7f014de109ebc1e99e51f3
Leo Wong <gnowoel@gmail.com>
2016-01-11 01:26:35 +0800

    Handling layouts when rendering a template

--- render/template.js
+++ render/template.js
@@ -4,9 +4,8 @@ var table = require('../table');
 function render(location) {
   return new Promise(function(resolve, reject) {
     var file = table.l2f(location);
-    var engine = file.meta('engine');
 
-    engine.render(file, {}, function(err, rendered) {
+    renderFile(file, {}, function(err, rendered) {
       if (err) return reject(err);
 
       var clone = file.clone();
@@ -15,6 +14,40 @@ function render(location) {
 
       resolve(clone);
     });
+  });
+}
+
+function renderFile(file, opts, cb) {
+  var engine = file.meta('engine');
+
+  if (!engine) {
+    return cb(new Error('Plugin not found for rendering ' + file.relative));
+  }
+
+  engine.render(file, opts, function(err, rendered) {
+    if (err) return cb(err);
+
+    var layout = file.meta('frontmatter').layout;
+
+    if (layout) {
+      opts.body = rendered;
+
+      layout = '_layouts/' + layout;
+
+      if (!path.extname(layout)) {
+        layout = layout + engine.extnames[0];
+      }
+
+      var layoutFile = table.record.r2f(layout);
+
+      if (!layoutFile) {
+        return cb(new Error('Layout file not found: ' + layout));
+      }
+
+      return renderFile(layoutFile, opts, cb);
+    }
+
+    return cb(null, rendered);
   });
 }
 
