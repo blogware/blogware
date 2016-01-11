e6a51c15632b8140043491bcf8a4111e02032b4e
Leo Wong <gnowoel@gmail.com>
2016-01-12 01:01:12 +0800

    Update the plugin registry

--- plugin.js
+++ plugin.js
@@ -1,6 +1,10 @@
 var packageJson = require('./package.json');
 
-var store = {};
+var requires = [];
+var registry = {};
+
+registry.type = {};
+registry.extname = {};
 
 function register() {
   var pattern = /^blogware-plugin-/;
@@ -14,13 +18,28 @@ function register() {
 }
 
 function registerPlugin(plugin) {
-  plugin.extnames.forEach(function(extname) {
-    store[extname] = plugin;
+  var type = registry.type;
+  var extname = registry.extname;
+
+  requires.push(plugin);
+
+  type[plugin.type] = type[plugin.type] || [];
+  type[plugin.type].push(plugin);
+
+  plugin.extnames.forEach(function(ext) {
+    extname[ext] = extname[ext] || [];
+    extname[ext].push(plugin);
   });
 }
 
-function lookup(extname) {
-  return store[extname];
+function lookup(name, group) {
+  group = group || 'extname';
+
+  if (registry[group][name]) {
+    return registry[group][name][0];
+  }
+
+  return;
 }
 
 exports.register = register;
