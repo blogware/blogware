2c67962ed75862624972922700f6fdb6525c99a6
Leo Wong <gnowoel@gmail.com>
2016-01-13 01:22:05 +0800

    Split the `collection` step
    One for checking the collection of the input file, one for updating the
    collection table.

--- pass/collect.js
+++ pass/collect.js
@@ -1,42 +1,19 @@
 var table = require('../table');
 
-var collections = [
-  'posts',
-  'pages'
-]
-
 function pass(file) {
   if (file.meta('type') !== 'document') {
     return file;
   }
 
-  var collection = getCollection(file);
+  var collection = file.meta('collection');
 
   if (!collection) {
     return file;
   }
 
-  file.meta('collection', collection);
   table.collection.add(collection, file);
 
   return file;
-}
-
-function getCollection(file) {
-  var relative = file.relative;
-
-  var collection;
-
-  for (var i = 0; i < collections.length; i++) {
-    var name = collections[i];
-
-    if (relative.indexOf('_' + name + '/') === 0) {
-      collection = name;
-      break;
-    }
-  }
-
-  return collection;
 }
 
 module.exports = pass;
--- pass/index.js
+++ pass/index.js
@@ -1,17 +1,18 @@
 var steps = [
-  'path',    // fixing base path of the input file
-  'patch',   // monkey patching to add the meta method
-  'filter',  // ignoring directories and unchanged files
-  'engine',  // determing the file processor
-  'type',    // asset, template or other
-  'matter',  // parsing frontmatter
-  'compile', // precompiling templates
-  'partial', // registering a partial
-  'record',  // caching the input file
-  'route',   // updating the routing table
-  'title',   // setting the page title
-  'config',  // updating the config table
-  'collect'  // updating the collection table (posts, pages, etc.)
+  'path',       // fixing base path of the input file
+  'patch',      // monkey patching to add the meta method
+  'filter',     // ignoring directories and unchanged files
+  'engine',     // determing the file processor
+  'type',       // asset, template or other
+  'matter',     // parsing frontmatter
+  'collection', // checking if it belongs to a collection
+  'compile',    // precompiling templates
+  'partial',    // registering a partial
+  'record',     // caching the input file
+  'route',      // updating the routing table
+  'title',      // setting the page title
+  'config',     // updating the config table
+  'collect'     // updating the collection table (posts, pages, etc.)
 ];
 
 function pass(file) {
