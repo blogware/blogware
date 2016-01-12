1ce8ee25aed6695a26ba0d021208880bc4abf78a
Leo Wong <gnowoel@gmail.com>
2016-01-12 22:13:13 +0800

    Update the `blogware` configs during bootstrapping

--- bootstrap.js
+++ bootstrap.js
@@ -1,7 +1,15 @@
+var packageJson = require('./package.json');
+var table = require('./table');
 var plugin = require('./plugin');
 
 function bootstrap() {
+  updateConfigs();
   registerPlugins();
+}
+
+function updateConfigs() {
+  var version = packageJson.version;
+  table.config.add('blogware', 'version', version);
 }
 
 function registerPlugins() {
