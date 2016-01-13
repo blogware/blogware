f0b925e7e344e90985a9498bb926ae0501d0b6a3
Leo Wong <gnowoel@gmail.com>
2016-01-13 16:16:51 +0800

    Rename the `collection` stop to `group`

--- pass/index.js
+++ pass/index.js
@@ -1,19 +1,19 @@
 var steps = [
-  'base',       // fixing base path of the input file
-  'patch',      // monkey patching to add the meta method
-  'filter',     // ignoring directories and unchanged files
-  'engine',     // determing the file processor
-  'type',       // asset, template or other
-  'matter',     // parsing frontmatter
-  'collection', // checking if it belongs to a collection
-  'compile',    // precompiling templates
-  'partial',    // registering a partial
-  'record',     // caching the input file
-  'date',       // setting the page date
-  'route',      // updating the routing table
-  'title',      // setting the page title
-  'config',     // updating the config table
-  'collect'     // updating the collection table (posts, pages, etc.)
+  'base',    // fixing base path of the input file
+  'patch',   // monkey patching to add the meta method
+  'filter',  // ignoring directories and unchanged files
+  'engine',  // determing the file processor
+  'type',    // asset, template or other
+  'matter',  // parsing frontmatter
+  'group',   // checking if it belongs to a collection
+  'compile', // precompiling templates
+  'partial', // registering a partial
+  'record',  // caching the input file
+  'date',    // setting the page date
+  'route',   // updating the routing table
+  'title',   // setting the page title
+  'config',  // updating the config table
+  'collect'  // updating the collection table (posts, pages, etc.)
 ];
 
 function pass(file) {
