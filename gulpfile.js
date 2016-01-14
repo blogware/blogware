4455a1ac11fddd6a00208833b9a469a045a383b1
Leo Wong <gnowoel@gmail.com>
2016-01-14 14:08:25 +0800

    Revert back to `browserSync.reload` for now
    `htmlInjector` seems unreliable when:
    
      * Managing history of the page (would go back to the original content)
      * Dislaying error messages (just idling)

--- gulpfile.js
+++ gulpfile.js
@@ -75,7 +75,7 @@ function handler(event, path) {
   var dest;
 
   dest = gulp.dest('_site');
-  dest.on('finish', htmlInjector);
+  dest.on('finish', browserSync.reload);
 
   src
     .pipe(stream())
