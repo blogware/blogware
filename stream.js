9cd83f865adcf2a5415d3a6583810b4412ee6ffd
Leo Wong <gnowoel@gmail.com>
2016-01-16 23:13:57 +0800

    Restructure the main stream

--- stream.js
+++ stream.js
@@ -2,7 +2,9 @@ var through = require('through2');
 var pass = require('./pass');
 
 function stream() {
-  return through.obj(function(file, _, cb) {
+  return through.obj(transform);
+
+  function transform(file, _, cb) {
     var self = this;
     pass(file)
       .then(function(file) {
@@ -13,7 +15,7 @@ function stream() {
         cb();
       })
       .catch(cb);
-  });
+  }
 }
 
 module.exports = stream;
