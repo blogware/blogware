525f40bbc31d574adbbd1066432e5771028d0ada
Leo Wong <gnowoel@gmail.com>
2016-01-29 18:35:12 +0800

    Extract the `renderFile` method into a separate module

--- render/document/homepage.js
+++ render/document/homepage.js
@@ -1,7 +1,7 @@
 var _ = require('lodash');
 var path = require('path');
 var table = require('../../table');
-var plugin = require('../../plugin');
+var renderFile = require('./render-file');
 
 function render(location, file) {
   return new Promise(function(resolve, reject) {
@@ -108,51 +108,6 @@ function l2p(location) {
   path2 = path2.replace(/index.html$/, '');
 
   return path2;
-}
-
-function renderFile(file, opts, cb) {
-  var engine = file.meta('engine');
-
-  if (!engine) {
-    return cb(new Error('Plugin not found for rendering ' + file.relative));
-  }
-
-  engine.render(file, opts, function(err, rendered) {
-    if (err) return cb(err);
-
-    var layout = file.meta('matter').layout;
-
-    if (layout) {
-      layout = '_layouts/' + layout;
-
-      if (!path.extname(layout)) {
-        var _engine = engine;
-
-        if (_engine.type !== 'template engine') {
-          _engine = plugin.lookup('template engine', 'type');
-        }
-
-        layout = layout + _engine.extnames[0];
-      }
-
-      var layoutFile = table.record.r2f(layout);
-
-      if (!layoutFile) {
-        return cb(new Error('Layout not found: ' + layout));
-      }
-
-      if (opts.__layout[layout]) {
-        return cb(new Error('Layout are circular referenced: ' + layout));
-      }
-
-      opts.__layout[layout] = true;
-      opts.body = rendered;
-
-      return renderFile(layoutFile, opts, cb);
-    }
-
-    return cb(null, rendered);
-  });
 }
 
 module.exports = render;
---
+++ render/document/render-file.js
@@ -0,0 +1,50 @@
+var path = require('path');
+var table = require('../../table');
+var plugin = require('../../plugin');
+
+function renderFile(file, opts, cb) {
+  var engine = file.meta('engine');
+
+  if (!engine) {
+    return cb(new Error('Plugin not found for rendering ' + file.relative));
+  }
+
+  engine.render(file, opts, function(err, rendered) {
+    if (err) return cb(err);
+
+    var layout = file.meta('matter').layout;
+
+    if (layout) {
+      layout = '_layouts/' + layout;
+
+      if (!path.extname(layout)) {
+        var _engine = engine;
+
+        if (_engine.type !== 'template engine') {
+          _engine = plugin.lookup('template engine', 'type');
+        }
+
+        layout = layout + _engine.extnames[0];
+      }
+
+      var layoutFile = table.record.r2f(layout);
+
+      if (!layoutFile) {
+        return cb(new Error('Layout not found: ' + layout));
+      }
+
+      if (opts.__layout[layout]) {
+        return cb(new Error('Layout are circular referenced: ' + layout));
+      }
+
+      opts.__layout[layout] = true;
+      opts.body = rendered;
+
+      return renderFile(layoutFile, opts, cb);
+    }
+
+    return cb(null, rendered);
+  });
+}
+
+module.exports = renderFile;
